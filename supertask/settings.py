bdf1617ba07424ae15be3bc7f8c91f618270ddb7
Jannyfer Tamagno <jannyfertamagno@gmail.com>
2025-07-23 09:39:13 -0300

    ajustando build.sh

--- supertask/settings.py
+++ supertask/settings.py
@@ -183,3 +183,19 @@ LOGGING = {
         },
     },
 }
+
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'  # Usa SQLite em memória para testes
+    }
+    
+    # Desabilita migrações para acelerar testes
+    class DisableMigrations:
+        def __contains__(self, item):
+            return True
+        
+        def __getitem__(self, item):
+            return None
+    
+    MIGRATION_MODULES = DisableMigrations()
